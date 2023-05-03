7d0b9991a91ac61442fc070b4a57e4b14e6e2f09
andm <github.com/andm4>
2023-05-02 23:26:43 -0500

    Final done

--- battlegame.py
+++ battlegame.py
@@ -2,6 +2,7 @@
 wizard = "Wizard"
 elf = "Elf"
 human = "Human"
+drago ="Dragon"
 
 wizard_hp = 70
 elf_hp = 100
@@ -25,18 +26,36 @@ while True:
         my_hp = wizard_hp
         my_damage = wizard_damage
         break
-    if character == "2":
+    elif character == "2":
         character = elf
         my_hp = elf_hp
         my_damage = elf_damage
         break
-    if character == "3":
+    elif character == "3":
         character = human
         my_hp = human_hp
         my_damage = human_damage
         break
-    print ("Unknown character")
+    else:
+        print ("Unknown character")
 
 print("You chose the: ", character)
 print("Health: ", my_hp)
-print("Damage: ", my_damage)
+print("Damage: ", my_damage, "\n")
+
+while True:
+    dragon_hp = dragon_hp - my_damage
+    my_hp = my_hp - dragon_damage
+    print("The", character, "damaged the Dragon!")
+    print("The Dragon's hit points are now:", dragon_hp, "\n")
+
+    if dragon_hp <= 0:
+        print("The Dragon has lost the battle")
+        break
+
+    print("The Dragon strikes back at", character)
+    print("The", character + "'s", "hitpoints are now:", my_hp, "\n")
+
+    if my_hp <= 0:
+        print("The", character, "has lost the fight for asgard")
+        break
