1ca655d7dcfadc015fe97b23b8fd648b0cdafc4a
andm <github.com/andm4>
2023-05-06 14:26:25 -0500

    Bonus: Repeat game

--- battlegame.py
+++ battlegame.py
@@ -18,57 +18,63 @@ dragon_damage = 50
 orc_damage = 200
 
 #Task 2 & 3
-while True:
-    print ("1) Wizard")
-    print ("2) Elf")
-    print ("3) Human")
-    print ("4) Orc")
-    print ("5) End Game")
-    character = input("Choose your character: ")
+play_again = "y"
+while play_again.lower() == "y":
+    while True:
+        print ("1) Wizard")
+        print ("2) Elf")
+        print ("3) Human")
+        print ("4) Orc")
+        print ("5) End Game")
+        character = input("Choose your character: ")
 
-    if character.lower() in ["1", "wizard"]:
-        character = wizard
-        my_hp = wizard_hp
-        my_damage = wizard_damage
-        break
-    elif character.lower() in ["2", "elf"]:
-        character = elf
-        my_hp = elf_hp
-        my_damage = elf_damage
-        break
-    elif character.lower() in ["3", "human"]:
-        character = human
-        my_hp = human_hp
-        my_damage = human_damage
-        break
-    elif character.lower() in ["4", "orc"]:
-        character = orc
-        my_hp = orc_hp
-        my_damage = orc_damage
-        break
-    elif character == "5":
-        print("Exiting the game.....")
-        exit()
-    else:
-        print ("Unknown character")
+        if character.lower() in ["1", "wizard"]:
+            character = wizard
+            my_hp = wizard_hp
+            my_damage = wizard_damage
+            break
+        elif character.lower() in ["2", "elf"]:
+            character = elf
+            my_hp = elf_hp
+            my_damage = elf_damage
+            break
+        elif character.lower() in ["3", "human"]:
+            character = human
+            my_hp = human_hp
+            my_damage = human_damage
+            break
+        elif character.lower() in ["4", "orc"]:
+            character = orc
+            my_hp = orc_hp
+            my_damage = orc_damage
+            break
+        elif character == "5":
+            print("Exiting the game.....")
+            exit()
+        else:
+            print ("Unknown character")
 
-print("You chose the: ", character)
-print("Health: ", my_hp)
-print("Damage: ", my_damage, "\n")
+    print("You chose the: ", character)
+    print("Health: ", my_hp)
+    print("Damage: ", my_damage, "\n")
 
-while True:
-    dragon_hp = dragon_hp - my_damage
-    my_hp = my_hp - dragon_damage
-    print("The", character, "damaged the Dragon!")
-    print("The Dragon's hit points are now:", dragon_hp, "\n")
+    while True:
+        dragon_hp = dragon_hp - my_damage
+        my_hp = my_hp - dragon_damage
+        print("The", character, "damaged the Dragon!")
+        print("The Dragon's hit points are now:", dragon_hp, "\n")
 
-    if dragon_hp <= 0:
-        print("The Dragon has lost the battle")
-        break
+        if dragon_hp <= 0:
+            print("The Dragon has lost the battle")
+            break
 
-    print("The Dragon strikes back at", character)
-    print("The", character + "'s", "hitpoints are now:", my_hp, "\n")
+        print("The Dragon strikes back at", character)
+        print("The", character + "'s", "hitpoints are now:", my_hp, "\n")
 
-    if my_hp <= 0:
-        print("The", character, "has lost the fight for asgard")
-        break
+        if my_hp <= 0:
+            print("The", character, "has lost the fight for asgard" "\n")
+            break 
+    
+    play_again = input("Thou thy want to play again?? (y/n)")
+    if play_again.lower() != "y":
+        print("The Dragon smites thee!!! Begone!!")
