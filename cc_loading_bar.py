1407e577222f27c8fdf05b92556b6e2a1d53b06a
andm <github.com/andm4>
2023-05-07 20:27:27 -0500

    Add conditional statements with comments, halfpoints

--- cc_loading_bar.py
+++ cc_loading_bar.py
@@ -1,2 +1,15 @@
 for amount_loaded in range(0, 105, 5):
-    print(amount_loaded)
+    if amount_loaded == 25:
+        print(amount_loaded)
+        print("1/4 of the way there")
+    elif amount_loaded == 50:
+        print(amount_loaded)
+        print("1/2 of the way there")
+    elif amount_loaded == 75:
+        print(amount_loaded)
+        print("3/4 of the way there")
+    elif amount_loaded == 100:
+        print(amount_loaded)
+        print("Loading completed!")
+    else:
+        print(amount_loaded)
