475cf2cf9acaa7da983f2ae86298bb15b20b4ed9
andm <github.com/andm4>
2023-05-12 00:30:34 -0500

    week 2 challange: Reverse name $

--- cc_reverse.py
+++ cc_reverse.py
@@ -1,4 +1,3 @@
-
-
 name = input("What is your name? ")
-print("Your name reversed is:", reverse(name))
+reverse_name = name[::-1]  # Creating a reverse variable using slice notation to reverse input from name variable [start, stop, step]
+print("Your name reversed is:", reverse_name) # modified the print statement to include correct variable callback
