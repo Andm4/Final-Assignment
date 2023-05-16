7afdc16e0761d2fb2ae7d6ebb60b5270d77baf81
andm <github.com/andm4>
2023-05-15 23:07:32 -0500

    Week 2: How To Modules

---
+++ my_module.py
@@ -0,0 +1,3 @@
+def greet(name):
+    print("Hello", name + "!")
+flavor = "mint chip"
---
+++ use_module.py
@@ -0,0 +1,4 @@
+from my_module import greet, flavor #importing the my_module file
+
+greet("Albert Einstein")
+print("My favorite ice cream flavor is", flavor)
