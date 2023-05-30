83054f0d3270d57d8f312414871cb0f08cbe663f
andm <16772041+Andm4@users.noreply.github.com>
2023-05-30 00:21:51 -0500

    Adding dice randomize with option 1

--- cc_dicegame.py
+++ cc_dicegame.py
@@ -1,7 +1,8 @@
 import random
 
 high_score = 0
-
+dice_1 = random.randint(1, 6)
+dice_2 = random.randint(1, 6)
 
 
 def dice_game():
@@ -14,3 +15,8 @@ while True:
     print("1) Roll Dice")
     print("2) Leave Game")
     choice = input("Enter your choice: ")
+
+    if choice == ("1"):
+        print("You roll a... ", dice_1)
+        print("You roll a... ", dice_2)
+        break
