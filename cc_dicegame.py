eb549fd09985cd8ed66cf9a3a3149d06c71d2bfd
andm <16772041+Andm4@users.noreply.github.com>
2023-05-30 00:03:54 -0500

    Test

--- cc_dicegame.py
+++ cc_dicegame.py
@@ -3,7 +3,14 @@ import random
 high_score = 0
 
 
+
 def dice_game():
 
+    dice_game()
 
-dice_game()
+
+while True:
+    print("Current High Score: ", high_score)
+    print("1) Roll Dice")
+    print("2) Leave Game")
+    choice = input("Enter your choice: ")
