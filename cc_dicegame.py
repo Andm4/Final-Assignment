3803df4a263da4a3b708f17b0d10ee8107597b5e
andm <16772041+Andm4@users.noreply.github.com>
2023-06-05 23:42:14 -0500

    Dice game challenge

--- cc_dicegame.py
+++ cc_dicegame.py
@@ -1,14 +1,21 @@
 import random
 
 high_score = 0
-dice_1 = random.randint(1, 6)
-dice_2 = random.randint(1, 6)
-
 
 def dice_game():
+    global high_score  # To access the global high_score variable
 
-    dice_game()
+    dice_1 = random.randint(1, 6)
+    dice_2 = random.randint(1, 6)
+    total = dice_1 + dice_2
 
+    print("\nYou roll a... ", dice_1)
+    print("You roll a... ", dice_2)
+    print("\nYou have rolled a total of: ", total)
+
+    if total > high_score:
+        high_score = total
+        print("New High Score: ", high_score)
 
 while True:
     print("Current High Score: ", high_score)
@@ -16,7 +23,10 @@ while True:
     print("2) Leave Game")
     choice = input("Enter your choice: ")
 
-    if choice == ("1"):
-        print("You roll a... ", dice_1)
-        print("You roll a... ", dice_2)
+    if choice == "1":
+        dice_game()
+    elif choice == "2":
         break
+    else:
+        print("Invalid choice. Please try again.")
+   
