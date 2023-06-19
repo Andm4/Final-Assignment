91371e4a323b3d85f1b312a8fd543a84b9ae0812
andm <16772041+Andm4@users.noreply.github.com>
2023-06-18 22:31:25 -0500

    Task3: Log in and prompt for menu

--- workshop2/app.py
+++ workshop2/app.py
@@ -9,8 +9,22 @@ def atm_menu(name):
     print("| 3.    Withdraw    | 4.    Logout       |")
     print("------------------------------------------")
 
+#Task 2 registration
 print("          === Automated Teller Machine ===          ")
-name = input("Enter name to register: ")
-pin = input("Enter PIN: ")
-balance = "$0"
+name = input("Enter name to register: ")  #declared a name input
+pin = input("Enter PIN: ") # Declared a pin input
+balance = "$0" # Balance Variable
 print(name, "has been registered with a starting balance of", balance)
+
+# Task 3 Log in and prompt for menu options
+print("          === Automated Teller Machine ===          ")
+print("LOGIN")
+
+while True:  # Infinite while loop
+    name_to_validate = input("Enter name to Login: ") # New variable to store name input
+    pin_to_validate = input("Enter PIN: ") # New variable for storing PIN input
+    if name_to_validate == name and pin_to_validate == pin: # If statement, AND operator to check if both conditions are true
+        print("Login successful!")
+        break  # break out of loop if TRUE
+    else:  # Print if False, restart loop.
+        print("invalid credentials")
