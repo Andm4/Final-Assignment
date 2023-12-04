37f87f8c5a7bc3ee72ded0af3e92881f60aad431
Andm4 <16772041+Andm4@users.noreply.github.com>
2023-12-03 21:23:29 -0600

    updated account.py to have deposit and withdraw function

--- workshop2/banking_pkg/account.py
+++ workshop2/banking_pkg/account.py
@@ -1,4 +1,12 @@
 # adding a function named balance
 def show_balance(balance):
     print(f"Your account balance is: ${balance: .2f}")
-    
+
+# Adding the function for deposit
+def deposit(balance):
+    amount = float(input("Enter amount for deposit"))    
+
+# Adding the function for withdraw
+def withdraw(balance):
+    amount = float(input("Enter amount to withdraw"))
+    return balance - amount
