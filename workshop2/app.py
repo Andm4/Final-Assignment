0d03c5d116760a2741870fd63328b122da5367bb
Andm4 <16772041+Andm4@users.noreply.github.com>
2023-12-13 22:42:52 -0600

    Redo of all to save input

--- workshop2/app.py
+++ workshop2/app.py
@@ -16,8 +16,8 @@ def atm_menu(name):
 print("          === Automated Teller Machine ===          ")
 name = input("Enter name to register: ")  #declared a name input
 pin = input("Enter PIN: ") # Declared a pin input
-balance = "$0" # Balance Variable
-print(name, "has been registered with a starting balance of", balance)
+user_data = {name: {"pin": pin, "balance": 0}} # Balance Variable
+print(f"{name} has been registered with a starting balance of ${user_data[name]['balance']}")
 
 # Task 3 Log in and prompt for menu options
 print("          === Automated Teller Machine ===          ")
@@ -26,18 +26,25 @@ print("LOGIN")
 while True:  # Infinite while loop
     name_to_validate = input("Enter name to Login: ") # New variable to store name input
     pin_to_validate = input("Enter PIN: ") # New variable for storing PIN input
-    if name_to_validate == name and pin_to_validate == pin: # If statement, AND operator to check if both conditions are true
+    if name_to_validate in user_data and pin_to_validate == user_data[name_to_validate]["pin"]: # If statement, AND operator to check if both conditions are true
         print("Login successful!")
         break  # break out of loop if TRUE
     else:  # Print if False, restart loop.
         print("invalid credentials")
+user_balance = user_data[name]["balance"]
 
-# Task 3 second while loop
-user = atm_menu(name) #dec variable named User with atm argument
-print = (user) # This will print the above argument
 
 while True:  # 2nd While loop after entering Login
+    user = atm_menu(name)
     option = input("Choose an option: ") # Added option input for menu
-    user_balance = 0
     if option == "1" or option.lower() == "balance":
         account.show_balance(user_balance)
+    elif option == "2" or option.lower() == "deposit":
+        user_balance = account.deposit(user_balance)
+        account.show_balance(user_balance)
+    elif option == "3" or option.lower() == "withdraw":
+        user_balance = account.withdraw(user_balance)
+        account.show_balance(user_balance)
+    elif option == "4" or option.lower() == "logout":
+        account.logout(name)
+        break
--- workshop2/banking_pkg/account.py
+++ workshop2/banking_pkg/account.py
@@ -4,9 +4,14 @@ def show_balance(balance):
 
 # Adding the function for deposit
 def deposit(balance):
-    amount = float(input("Enter amount for deposit"))    
+    amount = float(input("Enter amount for deposit: "))
+    return amount
 
 # Adding the function for withdraw
 def withdraw(balance):
     amount = float(input("Enter amount to withdraw"))
     return balance - amount
+
+# Adding a logout function
+def logout(name):
+    print("goodbye", name)
