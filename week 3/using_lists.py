0fb9caa90cbf26f82da87120cf39000a5e3eef6d
Andm4 <16772041+Andm4@users.noreply.github.com>
2023-12-18 22:25:06 -0600

    Week 3

---
+++ week 3/using_lists.py
@@ -0,0 +1,8 @@
+states = ["Washington", "Oregon", "California"]
+print(states[0])
+print(states[1])
+print(states[2])
+
+# print(states[-1]) # This will list the states in reverse
+# print(states[-2])
+# print(states[-3])
