66171552d56cda8f039ca42dabba8b243c58e0f6
Andm4 <16772041+Andm4@users.noreply.github.com>
2023-12-20 22:05:50 -0600

    List methods

--- week 3/using_lists.py
+++ week 3/using_lists.py
@@ -6,3 +6,19 @@ print(states[2])
 # print(states[-1]) # This will list the states in reverse
 # print(states[-2])
 # print(states[-3])
+
+states[2] = "Arizona"
+# print(states) # This replaces califorina with Arizona
+
+# print(len(states)) # finding the length of a list
+
+# using List Methods
+states.append("New York")
+print(states) # The append method adds an item to the end of the list
+
+states.pop()
+print(states) # The pop method removes the last item on the list
+
+states.pop(1)
+print(states) # same pop method but removes the item based on the index number oregon
+          
