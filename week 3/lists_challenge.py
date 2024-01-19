68184d1a17751251896c296661bc22f0beaa3391
Andm4 <16772041+Andm4@users.noreply.github.com>
2024-01-18 22:57:44 -0600

    Step one

--- week 3/lists_challenge.py
+++ week 3/lists_challenge.py
@@ -6,3 +6,8 @@ diamonds = ["AD", "2D", "3D", "4D", "5D", "6D",
 hand = []
 
 while diamonds:
+    choice = input("Press enter to pick a card, or Q then enter to quit: ") #prompting the user to make a choice witht he input function   
+    if choice == " ":
+        print("your hand: ", random.choices(diamonds))
+    if choice == "Q":
+        break
